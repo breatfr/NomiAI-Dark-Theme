4f09ac13050a1cfa5df15918f0bfa5477a1c9e7b
BreatFR <6360676+breatfr@users.noreply.github.com>
2023-08-12 18:17:44 +0200

    Update nomi.ai-new-features.js

--- nomi.ai-new-features.js
+++ nomi.ai-new-features.js
@@ -1,9 +1,12 @@
 // ==UserScript==
 // @name        Nomi AI - New features
+// @namespace
 // @match       https://beta.nomi.ai/*
-// @grant       none
 // @version     1.0
 // @author      BreatFR
+// @updateURL   https://raw.githubusercontent.com/breatfr/NomiAI-Dark-Theme/main/nomi.ai-new-features.js
+// @downloadURL https://raw.githubusercontent.com/breatfr/NomiAI-Dark-Theme/main/nomi.ai-new-features.js
+// icon         https://assets.nomi.ai/_next/static/media/favicon.49a32755.svg
 // @description New features for Nomi AI
 // ==/UserScript==
 
